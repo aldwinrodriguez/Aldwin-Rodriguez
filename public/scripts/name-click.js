677cb00d357da62feada8a797c035d6a1723e947
Aldwin Rodriguez <aldwin.rodriguez@owl.ucc.edu>
2019-04-30 04:27:19 -0400

    looking good :D

--- public/scripts/name-click.js
+++ public/scripts/name-click.js
@@ -1,39 +1,49 @@
-// $(function () {
-//     move();
-//     setTimeout(() => {
-//         move();
-//     }, 2700);
+$(function () {
+    $($('#home').prev()[0]).addClass('background-tranform');
+    
+    let $nav = $('#home div.nav');
+    let $name = $nav[0];
+    let $resume = $nav[1];
+    let $contact = $nav[2];
+    let $project = $nav[3];
+    let $social = $nav[4];
+    let $socialNav = $('#social .socialContainer div');
+    let $instagram = $socialNav[0];
+    let $facebook = $socialNav[1];
+    let $youtube = $socialNav[2];
+    let $github = $socialNav[3];
+    let $connect = $socialNav[4];
+    var i = 0;
+    var txt = 'Aldwin Rodriguez .';
 
-let $nav = $('#home div.nav');
-let $name = $nav[0];
-let $resume = $nav[1];
-let $contact = $nav[2];
-let $project = $nav[3];
-let $social = $nav[4];
+    typeWriter();
+    setTimeout(() => {
+        move();
+        setTimeout(() => {
+            move();
+        }, 2700);
+    }, 5500);
+    $($name).on('click', move);
 
-// });
+    function move() {
+        $($resume).toggleClass('resume-transform');
+        $($contact).toggleClass('contact-transform');
+        $($project).toggleClass('project-transform');
+        $($social).toggleClass('social-transform');
+        $($instagram).toggleClass('instagram-transform');
+        $($facebook).toggleClass('facebook-transform');
+        $($youtube).toggleClass('youtube-transform');
+        $($github).toggleClass('github-transform');
+        $($connect).toggleClass('connect-transform');
+    }
 
+    function typeWriter() {
+        if (i < txt.length) {
+            let letter = '<span>' + txt.charAt(i) + '</span>';
+            $($name).append(letter);
+            i++;
+            setTimeout(typeWriter, 300);
+        }
+    }
 
-let el = document.getElementById('name');
-el.addEventListener('click', move);
-
-
-let resume = el.nextElementSibling;
-let contact = resume.nextElementSibling;
-let project = contact.nextElementSibling;
-let social = project.nextElementSibling;
-let instagram = document.getElementById('instagram');
-let facebook = document.getElementById('facebook');
-let youtube = document.getElementById('youtube');
-let github = document.getElementById('github');
-
-function move(e) {
-    resume.classList.toggle("resume-transform");
-    contact.classList.toggle("contact-transform");
-    project.classList.toggle("project-transform");
-    social.classList.toggle("social-transform");
-    instagram.classList.toggle("instagram-transform");
-    facebook.classList.toggle("facebook-transform");
-    github.classList.toggle("github-transform");
-    youtube.classList.toggle("youtube-transform");
-}
+});
